9432df922863567183b8a26341d8cee3da60f42d
Shahjalal Farhan Bhuiyan <130706590+rhapsodicwaffle@users.noreply.github.com>
2025-05-06 23:08:07 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
+import os
 from flask import Flask
 from flask_socketio import SocketIO
-from routes.collab import collab, register_socketio_handlers
-from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from models import db
+from routes.collab import collab, register_socketio_handlers
 from routes.auth import auth
 from routes.capsule import capsule
 from routes.home import home
 from routes.delete_expired_capsules import delete_expired_capsules
 from routes.blog import blog  
 from routes.bookmark import bookmark_bp
-import config
 
 app = Flask(__name__)
-app.config.from_object(config)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+migrate = Migrate(app, db)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 app.register_blueprint(auth)
@@ -23,18 +26,13 @@ app.register_blueprint(home)
 app.register_blueprint(blog)
 app.register_blueprint(bookmark_bp)
 app.register_blueprint(collab)
-# Register socketio events
-register_socketio_handlers(socketio)  # ✅ Hook up real-time events
-
+register_socketio_handlers(socketio)
 
 @app.route('/')
 def index():
     return "<h1>Welcome to PastPort API</h1><p>Visit /login or /create to get started.</p>"
 
-
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
         delete_expired_capsules()
     app.run(debug=True)
-
